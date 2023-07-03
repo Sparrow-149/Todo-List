d1b21ef0ccfa3bd0ced8254c2a1b070b612df628
Sparrow-149 <amanjoshi149@gmail.com>
2023-07-03 16:37:50 +0530

    Added Database to the Project

--- app.js
+++ app.js
@@ -1,56 +1,126 @@
-// require modules
 const express = require("express");
 const bodyParser = require("body-parser");
-// this is for date.js the modulue which we created
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
 const date = require(__dirname + "/date.js");
 
-// creating an express app
+
 const app = express();
 
-// creating an array to store the tasks
-const newtasks = ["Buy Food","Cook Food","Eat Food"];
-const workItems = [];
 
-// setting up ejs as view engine
 app.set('view engine', 'ejs');
-// using body parser
 app.use(bodyParser.urlencoded({extended:true}));
-// using static files
 app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB"); //connection String
+const itemsSchema = {
+    name : String
+};
+const Item  = mongoose.model("Item",itemsSchema);
+const item1 = new Item({
+    name : "Welcome to your todolist!"
+});
+const item2 = new Item({
+    name : "Hit the + button to add a new item"
+});
+const item3 = new Item({
+    name : "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1,item2,item3];
+const ListSchema = {
+    name : String,
+    items : [itemsSchema]
+};
+
+const List = mongoose.model("List",ListSchema);
 
 // get request
 app.get("/",function(req,res){
-    // let day  = date(); // this is for date.js the modulue which we created
-    // this is useful when we have only one fucntion which is directly bounded to module.exports
-    // but if we have multiple functions we can use exports
     const day = date.getDate();
-    
-    res.render("list",{listTitle:day, newlistitems:newtasks});
+    Item.find().then(function(foundItems){
+        if(foundItems.length === 0){
+            Item.insertMany(defaultItems).then(function(){
+                console.log("Successfully inserted the default items");
+            });
+            res.redirect("/");
+        }
+        else
+            res.render("list",{listTitle:day, newlistitems:foundItems});
+    });
 });
-// get request for work route
-app.get("/work",function(req,res){
-    // this res.render is used to render the ejs file for the work route
-    res.render("list", {listTitle: "Work", newlistitems : workItems} );
-});
-// get request for about route
-app.get("/about",function(req,res){
-    res.render("about");
+
+// req params
+app.get("/:customListName",function(req,res){
+    const customListName = _.capitalize(req.params.customListName);
+    List.findOne({name:customListName}).then(function(foundList){
+        // console.log(foundList);
+        if(!foundList){
+            const list = new List({
+                name : customListName,
+                items : defaultItems
+            });
+            list.save();
+            res.redirect("/"+customListName);
+        }   
+        else{
+            if(foundList.items.length ===0)
+            {
+                foundList.items = defaultItems;
+                foundList.save();
+            }
+            res.render("list",{listTitle:foundList.name, newlistitems:foundList.items});
+        }
+    });
 });
 
 // post request
 app.post("/",function (req,res) {  
-    // fetching the new item from the form
     const newtask  = req.body.newitem;
-    // checking the list name if it is for work route or home route
-    if(req.body.list == "Work"){
-        workItems.push(newtask);
-        res.redirect("/work");
-    }else{
-        newtasks.push(newtask);
+    const listName = req.body.list;
+    const itemName = new Item({
+        name : newtask
+    });
+
+    if(listName === date.getDate()){
+        if(itemName.name != "")
+            itemName.save();
         res.redirect("/");
+    }
+    else{
+        List.findOne({name:listName}).then(function(foundList){
+            if(itemName.name != "")
+            {
+                foundList.items.push(itemName);
+                foundList.save();
+            }
+        });
+        res.redirect("/" + listName);
     }
 });
 
+
+
+app.post('/delete',function(req,res){
+    const checkedItemId = req.body.chkbox;
+    const listname = req.body.listname;
+    if(listname === date.getDate())
+    {
+        Item.deleteOne({_id:checkedItemId}).then(function(){
+            res.redirect("/");
+        });
+    }
+    else
+    {
+        List.findOneAndUpdate({ name: listname }, { $pull: {items:{_id:checkedItemId}}}).then(
+            function () {
+              res.redirect("/" + listname);
+            }
+          );
+    }
+});
 
 app.listen(3000,function(req,res){
     console.log("server Listening to port 3000");
